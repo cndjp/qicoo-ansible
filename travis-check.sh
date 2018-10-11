b4e9dd440d8f54bda640da9c0481e502056ce2ac
nnao45 <n4sekai5y@gmail.com>
2018-10-11 05:28:26 +0000

    ワンチャン

--- travis-check.sh
+++ travis-check.sh
@@ -8,5 +8,6 @@ for DIR in ${ARY[@]}; do
   done
 done
 
-cli/git-secrets/git-secrets --register-aws --global
-cli/git-secrets/git-secrets --scan . 
+make install -C cli/git-secrets/ 
+git secrets --register-aws --global
+git secrets --scan . 
