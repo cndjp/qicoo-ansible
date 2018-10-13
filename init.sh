defe5e1aaab998b23b124ad1fa7bade13d24c93a
nnao45 <n4sekai5y@gmail.com>
2018-10-13 14:12:07 +0000

    little-change

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 python files/get-pip.py --user
+echo 'PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
+echo 'alias a="ansible-playbook -i localhost, -c local"' >> ~/.bashrc
 pip install --user ansible
