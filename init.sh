0c6a4a525e7356afd9aac91f65df56d3bea085bf
nnao45 <n4sekai5y@gmail.com>
2018-10-14 10:48:43 +0000

    オマケ

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 python files/get-pip.py --user
 echo 'PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
 echo 'alias a="ansible-playbook -i localhost, -c local"' >> ~/.bashrc
+echo 'export KUBECONFIG=~/.kube/config' >> ~/.bashrc
 pip install --user ansible
 pip install --user boto3
 sudo apt-get install -y jq git
