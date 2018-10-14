bf83911c4ebe6fa70572b1a716a2842538c3637b
nnao45 <n4sekai5y@gmail.com>
2018-10-14 03:02:20 +0000

    stable eks spinnaker

--- init.sh
+++ init.sh
@@ -4,3 +4,4 @@ python files/get-pip.py --user
 echo 'PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
 echo 'alias a="ansible-playbook -i localhost, -c local"' >> ~/.bashrc
 pip install --user ansible
+sudo apt-get install -y jq git
