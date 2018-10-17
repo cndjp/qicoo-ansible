0c73b447da478b82f58efdbf27d847042270b853
nnao45 <n4sekai5y@gmail.com>
2018-10-17 09:21:27 +0000

    自動上げ下げシステムのバッチ

--- bat/all-down.sh
+++ bat/all-down.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ANSIBLE_CTL="ansible-playbook -i localhost, -c local"
+BACKUP_FLAGFILE="~/qicoo-backup-flag"
 
 ${ANSIBLE_CTL} ark/create-heptio-backup.yml
 ${ANSIBLE_CTL} eks/delete-eks-env.yml
 ${ANSIBLE_CTL} aws/delete-rds-instance.yml
 ${ANSIBLE_CTL} aws/delete-elasticache-cluster.yml
-${ANSIBLE_CTL} ark/delete-heptio-deployment.yml
