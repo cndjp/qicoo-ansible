aad2b16b0b1813424f67798786e465e8678120b4
nnao45 <n4sekai5y@gmail.com>
2018-10-17 11:57:25 +0000

    stable

--- bat/all-down.sh
+++ bat/all-down.sh
@@ -2,12 +2,13 @@
 
 ANSIBLE_CTL="ansible-playbook -i localhost, -c local"
 TIMESTAMP=$(date "+%Y%m%d%H%M%S")
-ALLDOWN_LOG_DIR="~/qicoo-all-down/"
+ALLDOWN_LOG_DIR="/home/qicoo/qicoo-all-down/"
 
-ALLDOWN_LOG_FILE+=${ALLDOWN_LOG_DIR}/
+ALLDOWN_LOG_FILE+=${ALLDOWN_LOG_DIR}
 ALLDOWN_LOG_FILE+=qicoo-all-down_${TIMESTAMP}.log
 
-${ANSIBLE_CTL} ark/create-heptio-backup.yml > ${ALLDOWN_LOG_FILE}
-${ANSIBLE_CTL} eks/delete-eks-env.yml > ${ALLDOWN_LOG_FILE}
-${ANSIBLE_CTL} aws/delete-rds-instance.yml > ${ALLDOWN_LOG_FILE}
-${ANSIBLE_CTL} aws/delete-elasticache-cluster.yml > ${ALLDOWN_LOG_FILE}
+touch ${ALLDOWN_LOG_FILE}
+${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/ark/create-heptio-backup.yml > ${ALLDOWN_LOG_FILE}
+${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/eks/delete-eks-env.yml > ${ALLDOWN_LOG_FILE}
+${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/aws/delete-rds-instance.yml > ${ALLDOWN_LOG_FILE}
+${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/aws/delete-elasticache-cluster.yml > ${ALLDOWN_LOG_FILE}
--- bat/all-up.sh
+++ bat/all-up.sh
@@ -2,14 +2,14 @@
 
 ANSIBLE_CTL="ansible-playbook -i localhost, -c local"
 TIMESTAMP=$(date "+%Y%m%d%H%M%S")
-ALLUP_LOG_DIR="~/qicoo-all-up/"
+ALLUP_LOG_DIR="/home/qicoo/qicoo-all-up/"
 
-ALLUP_LOG_FILE+=${ALLUP_LOG_DIR}/
+ALLUP_LOG_FILE+=${ALLUP_LOG_DIR}
 ALLUP_LOG_FILE+=qicoo-all-up_${TIMESTAMP}.log
 
-${ANSIBLE_CTL} eks/setup-eks-env.yml > ${ALLUP_LOG_FILE}
-${ANSIBLE_CTL} aws/create-rds-instance.yml > ${ALLUP_LOG_FILE}
-${ANSIBLE_CTL} aws/create-elasticache-cluster.yml > ${ALLUP_LOG_FILE}
-${ANSIBLE_CTL} ark/create-heptio-deployment.yml > ${ALLUP_LOG_FILE}
-${ANSIBLE_CTL} ark/create-heptio-restore.yml > ${ALLUP_LOG_FILE}
+${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/eks/setup-eks-env.yml > ${ALLUP_LOG_FILE}
+${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/aws/create-rds-instance.yml > ${ALLUP_LOG_FILE}
+${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/aws/create-elasticache-cluster.yml > ${ALLUP_LOG_FILE}
+${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/ark/create-heptio-deployment.yml > ${ALLUP_LOG_FILE}
+${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/ark/create-heptio-restore.yml > ${ALLUP_LOG_FILE}
 hal deploy apply
