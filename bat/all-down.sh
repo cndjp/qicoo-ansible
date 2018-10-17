dc6f238a6396c31138d9df57cb81a2df2ea643c9
nnao45 <n4sekai5y@gmail.com>
2018-10-17 09:35:06 +0000

    自動上げ下げのロギング

--- bat/all-down.sh
+++ bat/all-down.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 ANSIBLE_CTL="ansible-playbook -i localhost, -c local"
-BACKUP_FLAGFILE="~/qicoo-backup-flag"
+TIMESTAMP=$(date "+%Y%m%d%H%M%S")
+ALLDOWN_LOG_DIR="~/qicoo-all-down/"
 
-${ANSIBLE_CTL} ark/create-heptio-backup.yml
-${ANSIBLE_CTL} eks/delete-eks-env.yml
-${ANSIBLE_CTL} aws/delete-rds-instance.yml
-${ANSIBLE_CTL} aws/delete-elasticache-cluster.yml
+ALLDOWN_LOG_FILE+=${ALLDOWN_LOG_DIR}/
+ALLDOWN_LOG_FILE+=qicoo-all-down_${TIMESTAMP}.log
+
+${ANSIBLE_CTL} ark/create-heptio-backup.yml > ${ALLDOWN_LOG_FILE}
+${ANSIBLE_CTL} eks/delete-eks-env.yml > ${ALLDOWN_LOG_FILE}
+${ANSIBLE_CTL} aws/delete-rds-instance.yml > ${ALLDOWN_LOG_FILE}
+${ANSIBLE_CTL} aws/delete-elasticache-cluster.yml > ${ALLDOWN_LOG_FILE}
--- bat/all-up.sh
+++ bat/all-up.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 ANSIBLE_CTL="ansible-playbook -i localhost, -c local"
+TIMESTAMP=$(date "+%Y%m%d%H%M%S")
+ALLUP_LOG_DIR="~/qicoo-all-up/"
 
-${ANSIBLE_CTL} eks/setup-eks-env.yml
-${ANSIBLE_CTL} aws/create-rds-instance.yml
-${ANSIBLE_CTL} aws/create-elasticache-cluster.yml
-${ANSIBLE_CTL} ark/create-heptio-deployment.yml
-${ANSIBLE_CTL} ark/create-heptio-restore.yml
+ALLUP_LOG_FILE+=${ALLUP_LOG_DIR}/
+ALLUP_LOG_FILE+=qicoo-all-up_${TIMESTAMP}.log
+
+${ANSIBLE_CTL} eks/setup-eks-env.yml > ${ALLUP_LOG_FILE}
+${ANSIBLE_CTL} aws/create-rds-instance.yml > ${ALLUP_LOG_FILE}
+${ANSIBLE_CTL} aws/create-elasticache-cluster.yml > ${ALLUP_LOG_FILE}
+${ANSIBLE_CTL} ark/create-heptio-deployment.yml > ${ALLUP_LOG_FILE}
+${ANSIBLE_CTL} ark/create-heptio-restore.yml > ${ALLUP_LOG_FILE}
 hal deploy apply
