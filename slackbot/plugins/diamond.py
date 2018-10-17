f7a730dfb511721c57c4d66c504e4e830753a37f
nnao45 <n4sekai5y@gmail.com>
2018-10-17 17:13:22 +0000

    os.sytemなら動くっぽいn

--- slackbot/plugins/diamond.py
+++ slackbot/plugins/diamond.py
@@ -4,7 +4,7 @@ from slackbot.bot import respond_to     # @botname: で反応するデコーダ
 from slackbot.bot import default_reply  # 該当する応答がない場合に反応するデコーダ
 from datetime import datetime
 import subprocess
-
+import os
 
 @respond_to('テスト')
 def mention_func(message):
@@ -12,19 +12,15 @@ def mention_func(message):
 
 @respond_to('上げて')
 def mention_func(message):
-    cmd = '/home/qicoo/qicoo-ansible/bat/all-up.sh'
-
     message.send('クレイジーダイアモンドォオオオオオオオ！！！')
     message.send('完璧じゃねーか、デプロイをしてる最中だという事を除いてよ〜〜〜〜〜〜〜〜〜〜。')
-    subprocess.call( cmd.split() )
+    os.system('sudo -u qicoo /home/qicoo/qicoo-ansible/bat/all-up.sh')
     message.reply('デプロイ終わったんじゃあないか。')
 
 @respond_to('下げて')
 def mention_func(message):
-    cmd = '/home/qicoo/qicoo-ansible/bat/all-down.sh'
-
     message.send('クレイジーダイアモンドォオオオオオオオ！！！')
-    message.send('全部消す？そいつはグレートだぜ、ちょっと待ってな。')
-    subprocess.call( cmd.split() )
+    message.send('全部消す？そいつはグレートだぜ！！ちょっと待ってな・・・。')
+    os.system('sudo -u qicoo /home/qicoo/qicoo-ansible/bat/all-down.sh')
     message.reply('ほぅら、消し終わったぜ。')
 
