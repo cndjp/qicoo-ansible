abcaa4a3841086b45717d45841416251a3df6828
nnao45 <n4sekai5y@gmail.com>
2018-10-17 11:29:14 +0000

    おもちゃ完成

--- init.sh
+++ init.sh
@@ -5,5 +5,5 @@ echo 'PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
 echo 'alias a="ansible-playbook -i localhost, -c local"' >> ~/.bashrc
 echo 'export KUBECONFIG=~/.kube/config' >> ~/.bashrc
 pip install --user ansible
-pip install --user boto3
+pip install --user boto3 slackbot
 sudo apt-get install -y jq git
