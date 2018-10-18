04e60588fd1058ca44118c345d1724626366e6aa
nnao45 <n4sekai5y@gmail.com>
2018-10-18 06:04:29 +0000

    見せてimple

--- slackbot/slackbot_settings.py
+++ slackbot/slackbot_settings.py
@@ -3,8 +3,8 @@
 import subprocess
 
 # botアカウントのトークンを指定
-token = subprocess.check_output('cat /home/qicoo/diamond_slack_token',shell=True).split()
-API_TOKEN = token
+#token = subprocess.check_output('cat /home/qicoo/diamond_slack_token',shell=True)
+API_TOKEN = subprocess.check_output('cat /home/qicoo/diamond_slack_token',shell=True).split()
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "「上げて」 か 「下げて」って言ってくれよな！"
