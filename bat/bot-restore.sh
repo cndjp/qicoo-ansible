9fa609c2cc7bc024f08c6cb09af2431822b8e039
nnao45 <n4sekai5y@gmail.com>
2018-10-18 23:13:22 +0900

    戻してと覚えての処理にhalを追加

--- bat/bot-restore.sh
+++ bat/bot-restore.sh
@@ -5,4 +5,9 @@ ARKRESTORE_LOG_DIR="/home/qicoo/qicoo-ark-restore/"
 ARKRESTORE_LOG_FILE+=${ARKRESTORE_LOG_DIR}
 ARKRESTORE_LOG_FILE+=${1}
 
-${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/ark/create-heptio-restore.yml > ${ARKRESTORE_LOG_FILE}
+/home/qicoo/qicoo-ansible/bat/hal-restore.sh > ${ARKRESTORE_LOG_FILE}
+echo "hal deploy clean" >> ${ARKRESTORE_LOG_FILE}
+hal deploy clean -q >> ${ARKRESTORE_LOG_FILE}
+${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/ark/create-heptio-restore.yml >> ${ARKRESTORE_LOG_FILE}
+echo "hal deploy apply" >> ${ARKRESTORE_LOG_FILE}
+hal deploy apply -q >> ${ARKRESTORE_LOG_FILE}
--- bat/hal-backup.sh
+++ bat/hal-backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-hal backup create
+hal backup create -q
 mv /home/qicoo/halbackup-*.tar /home/qicoo/hal-back/
