c9d02fa90b2294778c0602d8872ee35b38427ecc
nnao45 <n4sekai5y@gmail.com>
2018-10-18 18:20:49 +0900

    　リストアする順を変更

--- bat/all-up.sh
+++ bat/all-up.sh
@@ -10,9 +10,9 @@ ALLUP_LOG_DIR="/home/qicoo/qicoo-all-up/"
 ALLUP_LOG_FILE+=${ALLUP_LOG_DIR}
 ALLUP_LOG_FILE+=${1}
 
-${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/aws/create-rds-instance.yml >> ${ALLUP_LOG_FILE}
-${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/aws/create-elasticache-cluster.yml >> ${ALLUP_LOG_FILE}
 ${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/eks/setup-eks-env.yml >> ${ALLUP_LOG_FILE}
 ${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/ark/create-heptio-deployment.yml >> ${ALLUP_LOG_FILE}
-${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/ark/create-heptio-restore.yml >> ${ALLUP_LOG_FILE}
+${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/aws/create-rds-instance.yml >> ${ALLUP_LOG_FILE}
+${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/aws/create-elasticache-cluster.yml >> ${ALLUP_LOG_FILE}
 hal deploy apply
+${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/ark/create-heptio-restore.yml >> ${ALLUP_LOG_FILE}
