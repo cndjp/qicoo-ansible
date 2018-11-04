65b39e1d2b5169a961084ad29e115401b94dfbb5
nnao45 <n4sekai5y@gmail.com>
2018-11-04 21:43:38 +0900

    東方仗助update

--- bat/all-up.sh
+++ bat/all-up.sh
@@ -5,10 +5,14 @@ ALLUP_LOG_DIR="/home/qicoo/qicoo-all-up/"
 ALLUP_LOG_FILE+=${ALLUP_LOG_DIR}
 ALLUP_LOG_FILE+=${1}
 
+if [ -z ${2} ]; then
+  ALLUP_ZENBU_FLAG+='--skip-tags "zenbu"'
+fi
+
 ${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/eks/setup-eks-env.yml --skip-tags "no-routine" >> ${ALLUP_LOG_FILE}
 ${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/ark/create-heptio-deployment.yml >> ${ALLUP_LOG_FILE}
-${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/aws/create-rds-instance.yml >> ${ALLUP_LOG_FILE}
-${ANSIBLE_CTL} /home/qicoo/qicoo-ansible/aws/create-elasticache-cluster.yml >> ${ALLUP_LOG_FILE}
+${ANSIBLE_CTL} ${ALLUP_ZENBU_FLAG} /home/qicoo/qicoo-ansible/aws/create-rds-instance.yml >> ${ALLUP_LOG_FILE}
+${ANSIBLE_CTL} ${ALLUP_ZENBU_FLAG} /home/qicoo/qicoo-ansible/aws/create-elasticache-cluster.yml >> ${ALLUP_LOG_FILE}
 echo "hal config restore" >> ${ALLUP_LOG_FILE}
 /home/qicoo/qicoo-ansible/bat/hal-restore.sh >> ${ALLUP_LOG_FILE}
 echo "hal deploy apply" >> ${ALLUP_LOG_FILE}
