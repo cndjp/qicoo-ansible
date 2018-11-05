cee2a213779ced787af4818141df213bf146d25a
nnao45 <n4sekai5y@gmail.com>
2018-11-05 16:35:17 +0900

    halconfigのリストアが最新を持ってきてなかったので変更

--- bat/hal-restore.sh
+++ bat/hal-restore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LAST_HAL_BACKUP+="/home/qicoo/hal-back/"
-LAST_HAL_BACKUP+=$(ls -1 /home/qicoo/hal-back/ | grep halbackup- | sort -r | head -n1)
+LAST_HAL_BACKUP+=$(ls -1t /home/qicoo/hal-back/ | grep halbackup- | head -n1)
 hal backup restore -q --backup-path ${LAST_HAL_BACKUP}
 hal config provider kubernetes account edit aws --kubeconfig-file /home/qicoo/.kube/config
 hal config provider kubernetes account edit kubernetes-master --kubeconfig-file /home/qicoo/.kube/config
