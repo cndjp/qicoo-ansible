8044442ff9853a0c448e35a74c2b2f2d5d259a2a
nnao45 <n4sekai5y@gmail.com>
2018-11-05 13:31:28 +0900

    hal-retoreする際にkubeconfigを最新の物を使うよう指定

--- bat/hal-restore.sh
+++ bat/hal-restore.sh
@@ -3,3 +3,5 @@
 LAST_HAL_BACKUP+="/home/qicoo/hal-back/"
 LAST_HAL_BACKUP+=$(ls -1 /home/qicoo/hal-back/ | grep halbackup- | sort -r | head -n1)
 hal backup restore -q --backup-path ${LAST_HAL_BACKUP}
+hal config provider kubernetes account edit aws --kubeconfig-file /home/qicoo/.kube/config
+hal config provider kubernetes account edit kubernetes-master --kubeconfig-file /home/qicoo/.kube/config
