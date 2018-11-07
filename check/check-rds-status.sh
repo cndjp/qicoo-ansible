c2202eee03ef4258171f86f9ae342fb90fea327e
nnao45 <n4sekai5y@gmail.com>
2018-11-07 19:06:18 +0900

    試してのだいたい

---
+++ check/check-rds-status.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CHECK_LOG_FILE=${1}
+
+function check_rds_status() {
+  RDS_STATUS=`aws rds describe-db-instances --db-instance-identifier ${1} | jq '.DBInstances[].DBInstanceStatus'`
+  echo "RDS for MySQL ${1} Status " >> ${CHECK_LOG_FILE}
+  if [ -z ${RDS_STATUS} ];then
+    echo '"May be not exists"' >> ${CHECK_LOG_FILE}
+  else
+    echo ${RDS_STATUS} >> ${CHECK_LOG_FILE}
+  fi
+  echo "" >> ${CHECK_LOG_FILE}
+}
+
+check_rds_status 'qicoo-rds-01'   
+check_rds_status 'qicoo-rds-s01'   
+check_rds_status 'qicoo-rds-d01'    
--- slackbot/plugins/diamond.py
+++ slackbot/plugins/diamond.py
@@ -46,6 +46,18 @@ def list2exec(cmdlist):
 def mention_func(message):
     message.reply('テストで失敗しても直せばいいのによぉ〜〜〜〜〜〜〜。')
 
+@respond_to('試して')
+def mention_func(message):
+    now = datetime.now()
+    now_str = now.strftime('%Y%m%d%H%M%S')
+    log_file = 'qicoo-ark-try_' + now_str + '.log'
+    log_file_path = '/home/qicoo/qicoo-ark-try/' + log_file
+    cmd1 = 'sudo -u qicoo /home/qicoo/qicoo-ansible/check/check-route53-record.sh ' + log_file_path
+    cmd2 = 'sudo -u qicoo /home/qicoo/qicoo-ansible/check/check-rds-status.sh ' + log_file_path
+    cmd3 = 'sudo -u qicoo /home/qicoo/qicoo-ansible/check/check-ecache-status.sh ' + log_file_path
+
+    file2slack(log_file, log_file_path)
+
 @respond_to('繋げて')
 def mention_func(message):
     i=0
