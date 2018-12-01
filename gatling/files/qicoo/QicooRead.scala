37b849b01e2c48de32713d24fdcfacdd7e1badd7
hhiroshell <hhiroshell@gmail.com>
2018-12-02 02:59:23 +0900

    シナリオファイルの軽いリファクタリングと、負荷試験手順書の追加

--- gatling/files/qicoo/QicooRead.scala
+++ gatling/files/qicoo/QicooRead.scala
@@ -3,16 +3,20 @@ package qicoo
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-class Qicoo extends Simulation {
+class QicooRead extends Simulation {
 
     object Configuration {
-        var SCENARIO_NAME = "Qicoo test"
-        var REQUEST_NAME = "List Question"
+        var SCENARIO_NAME = "Qicoo List"
+        var REQUEST_NAME = "List Questions"
         var PATH = "/v1/jkd1812/questions"
 
-        val BASE_URL = System.getProperty("baseUrl", "https://api.qicoo.tokyo")
+        val BASE_URL = System.getProperty("baseUrl", "https://api-s.qicoo.tokyo")
         val USERS : Long = java.lang.Long.valueOf(System.getProperty("users", "12"))
         val DURING : Int = java.lang.Integer.valueOf(System.getProperty("during", "12"))
+        val PARAM_START = System.getProperty("start", "1")
+        val PARAM_END = System.getProperty("end", "100")
+        val PARAM_SORT = System.getProperty("sort", "created_at")
+        val PARAM_ORDER = System.getProperty("order", "desc")
 
         val URL = BASE_URL + PATH
     }
@@ -21,8 +25,13 @@ class Qicoo extends Simulation {
         .exec(
             http(Configuration.REQUEST_NAME)
             .get(Configuration.URL)
-            .queryParamSeq(Seq(("start", "1"), ("end", "100"), ("sort", "created_at"), ("order", "desc"))))
-        .pause(1)
+            .queryParamSeq(Seq(
+                    ("start", Configuration.PARAM_START),
+                    ("end", Configuration.PARAM_END),
+                    ("sort", Configuration.PARAM_SORT),
+                    ("order", Configuration.PARAM_ORDER)
+                ))
+        ).pause(1)
 
     var httpProtocol = http
         .acceptHeader("application/json")
@@ -38,4 +47,3 @@ class Qicoo extends Simulation {
     ).protocols(httpProtocol)
 
 }
-
--- gatling/files/qicoo/QicooWrite.scala
+++ gatling/files/qicoo/QicooWrite.scala
@@ -6,15 +6,15 @@ import io.gatling.http.Predef._
 class QicooWrite extends Simulation {
 
     object Configuration {
-        var SCENARIO_NAME_POST = "Qicoo Write"
+        var SCENARIO_NAME_POST = "Qicoo Post"
         var SCENARIO_NAME_LIKE = "Qicoo Like"
         var REQUEST_NAME_POST = "Post a Question"
         var REQUEST_NAME_LIKE = "Like a Question"
         var BASE_PATH = "/v1/jkd1812/questions"
 
-        val BASE_URL = System.getProperty("baseUrl", "https://api.qicoo.tokyo")
-        val USERS_POST : Long = java.lang.Long.valueOf(System.getProperty("users_post", "12"))
-        val USERS_LIKE : Long = java.lang.Long.valueOf(System.getProperty("users_like", "12"))
+        val BASE_URL = System.getProperty("baseUrl", "https://api-s.qicoo.tokyo")
+        val USERS_POST : Long = java.lang.Long.valueOf(System.getProperty("postUsers", "12"))
+        val USERS_LIKE : Long = java.lang.Long.valueOf(System.getProperty("likeUsers", "12"))
         val DURING : Int = java.lang.Integer.valueOf(System.getProperty("during", "12"))
         val LIKE_QID = System.getProperty("likeQid", "fcfd4f19-0623-47cc-a078-c99d6112b6d4")
 
