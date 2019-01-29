978b21c099a442f5be8a253da2dab8f1ce36de48
nnao45 <n4sekai5y@gmail.com>
2019-01-29 13:53:01 +0900

    証明書更新のdocker化

---
+++ nginx/cert-update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+systemctl stop nginx
+docker run \
+  --rm \
+  -p "80:80" \
+  -v /etc/letsencrypt:/etc/letsencrypt \
+  -v /var/log/letsencrypt:/var/log/letsencrypt \
+  deliverous/certbot certonly \
+  --standalone \
+  --non-interactive \
+  --agree-tos \
+  --renew-by-default \
+  --preferred-challenges http \
+  --email 'n4sekai5y@gmail.com' \
+  -d 'spinnaker.qicoo.tokyo'
+systemctl start nginx
+
